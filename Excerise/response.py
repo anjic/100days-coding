87ae3a8f2e7f5a7dbfa097445bbff6049de62c53
anjaneyulu <chinnam.a@msystechnologies.com>
2017-12-21 16:36:06 +0530

    Added latest code of xio

--- Excerise/response.py
+++ Excerise/response.py
@@ -1,5 +1,6 @@
 import requests 
 import xml.dom.minidom
+import json
 
 r = requests.get("http://10.20.238.4/storage/arrays/USE2600053GOI05C",auth=('administrator', 'administrator'))
 
@@ -8,3 +9,6 @@ xml = xml.dom.minidom.parseString(xmlstring) # or xml.dom.minidom.parseString(xm
 pretty_xml_as_string = xml.toprettyxml()
 print xml
 print pretty_xml_as_string
+print pretty_xml_as_string.json
+
+
